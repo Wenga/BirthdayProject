bc882613b768d616491e73450bbd1777d30fe0c3
Wenga <mankayanwenga@gmail.com>
2023-09-29 00:57:27 -0700

    Added overlay

---
+++ Overlay.js
@@ -0,0 +1,15 @@
+ // JavaScript to show the overlay when the page loads
+ window.addEventListener('load', () => {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'block';
+});
+
+// JavaScript to close the overlay with a fade-out effect when the "Close Overlay" button is clicked
+    const closeOverlayButton = document.getElementById('closeOverlayButton');
+    closeOverlayButton.addEventListener('click', () => {
+        const overlay = document.getElementById('overlay');
+        overlay.classList.add('overlay-hidden'); // Apply the hidden class with opacity: 0
+        setTimeout(function () {
+            overlay.remove();
+        }, 2000);
+    });
